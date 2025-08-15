2b4abee0155cbbcd29bb211b60f3502cce36a31f
anik804 <chakrabortyanik234@gmail.com>
2025-08-16 00:25:19 +0600

    making the navbar sticky

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-amber-400 shadow-sm">
+    <div className="navbar bg-amber-400 shadow-sm sticky top-0 z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -111,7 +111,10 @@ const Navbar = () => {
               <div className="w-10 rounded-full">
                 <img
                   alt="User Avatar"
-                  src={user.photoURL || "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"}
+                  src={
+                    user.photoURL ||
+                    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  }
                 />
               </div>
             </div>
@@ -119,14 +122,22 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li><a onClick={() => navigate('/dashboard/add-pet')}>Dashboard</a></li>
-              <li><a onClick={handleLogout}>Logout</a></li>
+              <li>
+                <a onClick={() => navigate("/dashboard/add-pet")}>Dashboard</a>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
             </ul>
           </div>
         ) : (
           <>
-            <Link to="register" className="btn btn-soft btn-warning ml-4">Register</Link>
-            <Link to="login" className="btn btn-soft btn-success ml-4">Login</Link>
+            <Link to="register" className="btn btn-soft btn-warning ml-4">
+              Register
+            </Link>
+            <Link to="login" className="btn btn-soft btn-success ml-4">
+              Login
+            </Link>
           </>
         )}
       </div>
